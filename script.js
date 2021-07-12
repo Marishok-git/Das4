17d557f7504a73076c262388e02f7e957e8e75b0
Marishok-git <ghmari@yandex.ru>
2021-07-12 14:30:08 +0400

    das4 commit

---
+++ script.js
@@ -0,0 +1,98 @@
+
+
+// // -----------------------DAs4 ex1--------------
+
+// var arr=["Karen", "Hayk", "Ani"];
+// // arr[0] = "Karen"
+// console.log(arr);
+
+// var arr=["Karen", "Hayk", "Ani"];
+// for (var i=arr; i<arr.lenght; i++){
+//     console.log(arr(i))
+// }
+
+
+
+// // -----------------------DAs4 ex2--------------
+
+
+// var arr=[1,15,25,100, 150, 200, 500, 300, 52, 927];
+// var max = arr[0]
+
+//  for(var i=0; i < arr.length; i++) {
+//     if (arr[i] >= max){
+//          max=arr[i]
+//      }
+    
+// }
+// console.log(max);
+
+
+// // -----------------------DAs4 ex3--------------
+
+
+// var arr=[1,15,25,100, 150, 200, 500, 300, 52, 927];
+// var min = arr[0]
+
+//  for(var i=0; i > arr.length; i++) {
+//     if (arr[i] <= min){
+//          min=arr[i]
+//      }
+    
+// }
+// console.log(min);
+
+
+
+// // -----------------------DAs4 ex4--------------
+
+// var arr=[1,15,25,100,150,200,500,30,52,92];
+// var min = arr[0]
+
+// for (var i=0; i < arr.length; i++) {
+//     if (arr[i]%2 <= min) {
+//         min = arr[i] 
+
+//     }
+
+// }console.log(min)
+
+
+
+// // -----------------------DAs4 ex5--------------
+
+// var arr=[1,15,25,100,150,200,500,30,52,92];
+
+// for(var i=arr.length-1; i>=0; i--){
+//             console.log(arr[i])
+// }
+
+
+
+
+
+// // -----------------------DAs4 ex6--------------
+
+// var arr=[1,2,8,12,0,11,4,0,10,3,7]
+// sum = arr[0]
+
+// for (var i=0; i < arr.length; i++) {
+//     if (arr[i] +- 1 > 8) {
+//         sum = arr[i] 
+
+//     }
+// } console.log(sum)
+
+
+
+// version 2:
+
+// var arr=[1,2,8,12,0,11,4,0,10,3,7]
+// sum = arr[0]
+
+// for (var i=0; i < arr.length; i++) {
+//     if (arr[arr.length] +- 1 > 8) {
+//         sum = arr[arr.length] 
+
+//     }
+// } console.log(sum)
